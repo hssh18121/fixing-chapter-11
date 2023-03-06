c835315930836611f15aa71843c833578f38ca54
hason-pixtastock <son.nguyenha@pixta.co.jp>
2023-03-06 11:03:29 +0700

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
-     
+    def full_title(page_title = '')
+        base_title = t("general_page_title")
+        if page_title.empty?
+        base_title
+        else
+        page_title + " | " + base_title
+        end
+    end
 end
