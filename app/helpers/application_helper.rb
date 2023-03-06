09d19f1a14e5da8816b2433868ac01091e530216
hason-pixtastock <son.nguyenha@pixta.co.jp>
2023-03-06 13:30:09 +0700

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
     def full_title(page_title = '')
         base_title = t("general_page_title")
         if page_title.empty?
-        base_title
+            base_title
         else
-        page_title + " | " + base_title
+            page_title + " | " + base_title
         end
     end
 end
