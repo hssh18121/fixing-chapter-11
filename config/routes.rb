13ab422fb379ab417c10d37eed9221339e4edf51
hason-pixtastock <son.nguyenha@pixta.co.jp>
2023-03-06 10:16:01 +0700

    Remove redundant files and add I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    root 'static_pages#home'
+    get 'static_pages/home'
+    get 'static_pages/help'
+    get 'static_pages/about'
+    get 'static_pages/contact'
+  end
 end
