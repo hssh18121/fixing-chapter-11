6cd697cad515b9569d38cbecf53d2370bbdbec0c
hason-pixtastock <son.nguyenha@pixta.co.jp>
2023-03-13 16:23:37 +0700

    Finish CRUD User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,27 @@
 class ApplicationController < ActionController::Base
     before_action :set_locale
+    before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user, only: [:edit, :update]
+
     def set_locale
         I18n.locale = params[:locale] || I18n.default_locale
     end
 
     def default_url_options
         {locale: I18n.locale}
+    end
+
+    def logged_in_user
+        unless logged_in?
+            store_location
+            flash[:danger] = "Please log in."
+            redirect_to login_url
+        end
+    end
+
+    def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
     end
 
     include SessionsHelper
